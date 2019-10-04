bddaeb4b6d7fec99ca78ff0be1751a5d0dd98d24
salama232 <salama232@hotmail.com>
2019-10-04 20:03:33 +0200

    init

---
+++ src/main/java/com/SpringBoot/Docker/flairstech/FlairstechApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Docker.flairstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlairstechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlairstechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Docker/flairstech/FlairstechApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SpringBoot.Docker.flairstech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlairstechApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
